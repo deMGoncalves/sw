807c8ccf268e3d54013d1fbd7eb827bbdd000a04
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-15 12:14:54 -0300

    feat(sw): inclusao do evento before install prompt

---
+++ src/beforeInstallPrompt.js
@@ -0,0 +1,3 @@
+window.addEventListener('beforeinstallprompt', (event) =>
+  event.userChoice((choiceResult) =>
+    console.log('SW beforeinstallprompt', choiceResult.outcome)))
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import * as f from 'f'
 import agent from 'agent'
 import env from 'env'
 import register from './register'
+import './beforeInstallPrompt'
 
 f.and(
   agent.isUser,
