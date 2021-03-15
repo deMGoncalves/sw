39be4e976d3f322a4a9c789bbe8c3860914dc80e
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-15 12:32:49 -0300

    style(beforeinstall): estilizacao da mensagem no console

--- src/beforeInstallPrompt.js
+++ src/beforeInstallPrompt.js
@@ -1,3 +1,3 @@
 window.addEventListener('beforeinstallprompt', (event) =>
   event.userChoice.then((choiceResult) =>
-    console.log('SW beforeinstallprompt', choiceResult.outcome)))
+    console.log('SW before install prompt: ', choiceResult.outcome)))
