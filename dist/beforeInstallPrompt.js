e56d5515455994016938bf6a36c357ccf2f2c749
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-15 14:50:06 -0300

    build(dist): automatic new publish

--- dist/beforeInstallPrompt.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-window.addEventListener('beforeinstallprompt', (event) => event.userChoice.then((choiceResult) => console.log('SW before install prompt: ', choiceResult.outcome)));
--- dist/index.js
+++ dist/index.js
@@ -2,5 +2,4 @@ import * as f from 'f';
 import agent from 'agent';
 import env from 'env';
 import register from "./register";
-import "./beforeInstallPrompt";
 f.and(agent.isUser, env.isProd, f.has('serviceWorker', navigator)) && register();
